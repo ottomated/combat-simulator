8410cc003ead8b35bc1148537475d9b79b94f711
Ottomated <otto@ottomated.net>
2020-08-28 23:20:27 -0700

    Add GA

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,38 +4,47 @@ import { ServerStyleSheets } from '@material-ui/core/styles';
 import theme from '../src/theme';
 
 export default class MyDocument extends Document {
-  render() {
-    return (
-      <Html lang="en">
-        <Head>
-          <meta name="theme-color" content={theme.palette.primary.main} />
-          <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-          />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+	render() {
+		return (
+			<Html lang="en">
+				<Head>
+					<meta name="theme-color" content={theme.palette.primary.main} />
+					<link
+						rel="stylesheet"
+						href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+					/>
+					<script async src="https://www.googletagmanager.com/gtag/js?id=UA-112279351-3"></script>
+					<script>
+						{`
+							window.dataLayer = window.dataLayer || [];
+							function gtag(){dataLayer.push(arguments);}
+							gtag(\`js\`, new Date());
+							gtag(\`config\`, \`UA-112279351-3\`);
+						`}
+					</script>
+				</Head>
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
+	}
 }
 
 MyDocument.getInitialProps = async (ctx) => {
-  const sheets = new ServerStyleSheets();
-  const originalRenderPage = ctx.renderPage;
+	const sheets = new ServerStyleSheets();
+	const originalRenderPage = ctx.renderPage;
 
-  ctx.renderPage = () =>
-    originalRenderPage({
-      enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
-    });
+	ctx.renderPage = () =>
+		originalRenderPage({
+			enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+		});
 
-  const initialProps = await Document.getInitialProps(ctx);
+	const initialProps = await Document.getInitialProps(ctx);
 
-  return {
-    ...initialProps,
-    styles: [...React.Children.toArray(initialProps.styles), sheets.getStyleElement()],
-  };
+	return {
+		...initialProps,
+		styles: [...React.Children.toArray(initialProps.styles), sheets.getStyleElement()],
+	};
 };
